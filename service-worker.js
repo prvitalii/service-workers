92dd1062cfda74712820873df4a289304604d8c7
Vitalii Prokaziuk <prokazyk.v@gmail.com>
2018-02-06 16:44:27 +0200

    commenting out the links

--- service-worker.js
+++ service-worker.js
@@ -6,8 +6,8 @@ this.addEventListener('install', function(event) {
         '/index.html',
         '/styles.css',
         '/app.js',
-        'https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fvelokyiv%2Fposts%2F1575379525842547',
-        'https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fpermalink.php%3Fstory_fbid%3D604314139910596%26id%3D291232054552141',
+        // 'https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fvelokyiv%2Fposts%2F1575379525842547',
+        // 'https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fpermalink.php%3Fstory_fbid%3D604314139910596%26id%3D291232054552141',
       ]);
     })
   );
@@ -25,25 +25,7 @@ this.addEventListener('fetch', function(event) {
       });
       return response.clone();
     }).catch(function() {
-      // return caches.match('/gallery/myLittleVader.jpg');
+      
     })
   );
 });
-
-// self.addEventListener('install', function(event) {
-//   // Perform install step:  loading each required file into cache
-//   event.waitUntil(
-//     caches.open('v1')
-//       .then(function(cache) {
-//         // Add all offline dependencies to the cache
-//         console.log("offline");
-//         // var title = document.getElementById("title");
-//         // title.style.display = "none";
-//         // return cache.addAll(REQUIRED_FILES);
-//       })
-//       .then(function() {
-//         // At this point everything has been cached
-//         return self.skipWaiting();
-//       })
-//   );
-// });
