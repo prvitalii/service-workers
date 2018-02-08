1d17b78b637e70e578c0fc97be21bc23990c4edc
Vitalii Prokaziuk <prokazyk.v@gmail.com>
2018-02-08 20:48:29 +0200

    offline ver + zurb

---
+++ zubr_test/app.js
@@ -0,0 +1,10 @@
+if (navigator.serviceWorker.controller) {
+  console.log('[PWA Builder] active service worker found, no need to register')
+} else {
+  navigator.serviceWorker.register('zurb-sw.js', {
+    scope: './'
+  }).then(function(reg) {
+    console.log('Service worker has been registered for scope:'+ reg.scope);
+  });
+}
+
--- zubr_test/zurb-sw.js
+++ zubr_test/zurb-sw.js
@@ -24,6 +24,7 @@ self.addEventListener('fetch', function(event) {
   event.respondWith(
     fetch(event.request).catch(function(error) {
       console.log( '[PWA Builder] Network request Failed. Serving content from cache: ' + error );
+
       return caches.open('pwabuilder-offline').then(function (cache) {
         return cache.match(event.request).then(function (matching) {
           var report =  !matching || matching.status == 404?Promise.reject('no-match'): matching;
